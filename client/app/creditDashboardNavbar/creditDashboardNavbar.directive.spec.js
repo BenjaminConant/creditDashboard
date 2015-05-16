a52efaa42367cc708ca6df11be1fd810d9248e2e
Benjamin Conant <conantbenjamin@gmail.com>
2015-05-16 13:23:47 -0400

    navbar now directive ...logic moved to link function

---
+++ client/app/creditDashboardNavbar/creditDashboardNavbar.directive.spec.js
@@ -0,0 +1,21 @@
+'use strict';
+
+describe('Directive: creditDashboardNavbar', function () {
+
+  // load the directive's module and view
+  beforeEach(module('creditDashboardApp'));
+  beforeEach(module('app/creditDashboardNavbar/creditDashboardNavbar.html'));
+
+  var element, scope;
+
+  beforeEach(inject(function ($rootScope) {
+    scope = $rootScope.$new();
+  }));
+
+  it('should make hidden element visible', inject(function ($compile) {
+    element = angular.element('<credit-dashboard-navbar></credit-dashboard-navbar>');
+    element = $compile(element)(scope);
+    scope.$apply();
+    expect(element.text()).toBe('this is the creditDashboardNavbar directive');
+  }));
+});
